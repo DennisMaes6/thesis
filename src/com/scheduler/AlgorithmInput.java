3df208fa83d142cd633f981fece9865f6bc265e4
joren.sjongers <joren.sjongers@icloud.com>
2020-12-06 17:41:13 +0100

    add AlgorithmInput

--- src/com/scheduler/AlgorithmInput.java
+++ src/com/scheduler/AlgorithmInput.java
@@ -1,4 +1,15 @@
 package com.scheduler;
 
+import com.scheduler.assistants.Assistant;
+import com.scheduler.shifttype.ShiftType;
+
+import java.util.List;
+
 public class AlgorithmInput {
+
+    List<Assistant> assistants;
+
+    List<Day> days;
+
+    List<ShiftType> shiftTypes;
 }
