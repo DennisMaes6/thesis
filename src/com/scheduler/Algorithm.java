616d031227e7de7093af22e6d108abc0d59e0d65
joren.sjongers <joren.sjongers@icloud.com>
2020-12-06 13:48:31 +0100

    add basic classes

---
+++ src/com/scheduler/Algorithm.java
@@ -0,0 +1,9 @@
+package com.scheduler;
+
+public class Algorithm {
+
+    public Schedule generateSchedule(AlgorithmInput input) {
+        // TODO: implement this.
+        return new Schedule();
+    }
+}
---
+++ src/com/scheduler/Schedule.java
@@ -0,0 +1,13 @@
+package com.scheduler;
+
+import java.util.Map;
+
+public class Schedule {
+
+    Map<Person, Roster> schedule;
+
+    public float score() {
+        // TODO: implement this.
+        return 0;
+    }
+}
