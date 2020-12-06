191754111d257fe77a76f3639d5bf4cb2df14b98
joren.sjongers <joren.sjongers@icloud.com>
2020-12-06 17:57:16 +0100

    add individualSchedule field

--- src/com/scheduler/schedule/IndividualSchedule.java
+++ src/com/scheduler/schedule/IndividualSchedule.java
@@ -1,5 +1,12 @@
 package com.scheduler.schedule;
 
+import com.scheduler.day.Day;
+import com.scheduler.shifttype.ShiftType;
+
+import java.util.Map;
+
 public class IndividualSchedule {
+
+    private Map<Day, ShiftType> individualSchedule;
 
 }
