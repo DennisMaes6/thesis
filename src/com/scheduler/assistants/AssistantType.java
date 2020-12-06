3b0c2782c9632ebbec0054e7477d366519cf5c52
joren.sjongers <joren.sjongers@icloud.com>
2020-12-06 17:41:36 +0100

    add AssistantType

---
+++ src/com/scheduler/assistants/AssistantType.java
@@ -0,0 +1,14 @@
+package com.scheduler.assistants;
+
+import com.scheduler.shifttype.ShiftType;
+
+import java.util.List;
+
+public enum AssistantType {
+    JA,
+    JA_F,
+    SA,
+    SA_F,
+    SA_NEO,
+    SA_F_NEO
+}
