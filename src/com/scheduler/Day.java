d289ba07a4c98073081a8d023bc2cb78b8b2404b
joren.sjongers <joren.sjongers@icloud.com>
2020-12-06 17:46:10 +0100

    add day

---
+++ src/com/scheduler/Day.java
@@ -0,0 +1,19 @@
+package com.scheduler;
+
+import java.util.Date;
+
+public class Day {
+
+    private Date date;
+
+    public boolean isWeekend() {
+        // TODO: implement
+        return false;
+    }
+
+    public boolean isHoliday() {
+        // TODO: implement
+        return false;
+    }
+
+}
