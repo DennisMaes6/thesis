c442da25d57713fb15c49fe017c2dfffa90150dd
joren.sjongers <joren.sjongers@icloud.com>
2020-12-13 14:32:11 +0100

    add mock input data

--- src/com/scheduler/main/Main.java
+++ src/com/scheduler/main/Main.java
@@ -1,26 +1,86 @@
 package com.scheduler.main;
 
+import com.scheduler.algorithm.Algorithm;
 import com.scheduler.algorithm.AlgorithmInput;
+import com.scheduler.assistant.Assistant;
+import com.scheduler.assistant.AssistantType;
+import com.scheduler.schedule.Schedule;
 import com.scheduler.shifttype.*;
+import com.scheduler.time.Day;
+import com.scheduler.time.Week;
 
+import java.text.ParseException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
-    List<ShiftType> shiftTypes = Arrays.asList(
-        new JuniorAssistantEvening(),
-        new JuniorAssistantNightWeek(),
-        new JuniorAssistantWeekendHoliday(),
-        new SeniorAssistantEveningWeek(),
-        new SeniorAssistantWeekendHoliday(),
-        new Transport(),
-        new Call()
-    );
+    public static void main(String[] args) throws ParseException {
+        List<Assistant> assistants = Arrays.asList(
+                new Assistant("assistant 1", AssistantType.JA, new HashSet<>()),
+                new Assistant("assistant 2", AssistantType.JA, new HashSet<>()),
+                new Assistant("assistant 3", AssistantType.JA, new HashSet<>()),
+                new Assistant("assistant 4", AssistantType.JA_F, new HashSet<>()),
+                new Assistant("assistant 5", AssistantType.JA_F, new HashSet<>()),
+                new Assistant("assistant 6", AssistantType.JA_F, new HashSet<>()),
+                new Assistant("assistant 7", AssistantType.SA, new HashSet<>()),
+                new Assistant("assistant 8", AssistantType.SA, new HashSet<>()),
+                new Assistant("assistant 9", AssistantType.SA, new HashSet<>()),
+                new Assistant("assistant 10", AssistantType.SA_F, new HashSet<>()),
+                new Assistant("assistant 11", AssistantType.SA_F, new HashSet<>()),
+                new Assistant("assistant 12", AssistantType.SA_F, new HashSet<>()),
+                new Assistant("assistant 13", AssistantType.SA_NEO, new HashSet<>()),
+                new Assistant("assistant 14", AssistantType.SA_NEO, new HashSet<>()),
+                new Assistant("assistant 15", AssistantType.SA_NEO, new HashSet<>()),
+                new Assistant("assistant 15", AssistantType.SA_F_NEO, new HashSet<>()),
+                new Assistant("assistant 15", AssistantType.SA_F_NEO, new HashSet<>()),
+                new Assistant("assistant 15", AssistantType.SA_F_NEO, new HashSet<>())
+        );
 
-    AlgorithmInput input = new AlgorithmInput();
+        List<Week> weeks = Arrays.asList(
+            new Week(1,
+                Arrays.asList(
+                    new Day(2020, 1, 1),
+                    new Day(2020, 1, 2),
+                    new Day(2020, 1, 3),
+                    new Day(2020, 1, 4),
+                    new Day(2020, 1, 5),
+                    new Day(2020, 1, 6),
+                    new Day(2020, 1, 7)
+                )
+            ),
+            new Week(1,
+                Arrays.asList(
+                    new Day(2020, 1, 8),
+                    new Day(2020, 1, 9),
+                    new Day(2020, 1, 10),
+                    new Day(2020, 1, 11),
+                    new Day(2020, 1, 12),
+                    new Day(2020, 1, 13),
+                    new Day(2020, 1, 14)
+                )
+            )
+        );
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+        Set<ShiftType> shiftTypes = new HashSet<>(
+            Arrays.asList(
+                new JuniorAssistantEvening(),
+                new JuniorAssistantNightWeek(),
+                new JuniorAssistantWeekendHoliday(),
+                new SeniorAssistantEveningWeek(),
+                new SeniorAssistantWeekendHoliday(),
+                new Transport(),
+                new Call()
+            )
+        );
+
+        AlgorithmInput input = new AlgorithmInput(assistants, weeks, shiftTypes);
+        Algorithm algorithm = new Algorithm(input);
+
+        Schedule schedule = algorithm.generateSchedule();
+
+        System.out.println(schedule.toString());
     }
 }
--- src/com/scheduler/time/Day.java
+++ src/com/scheduler/time/Day.java
@@ -1,30 +1,32 @@
 package com.scheduler.time;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Day {
 
     private Date date;
-    private Week week;
 
-    public Day(Date date, Week week) {
-        this.date = date;
-        this.week = week;
+    public Day(int year, int month, int day) throws ParseException {
+        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
+
+        String builder = "%s-%s-%s".formatted(
+                String.format("%1$4s", year),
+                String.format("%1$2s", month),
+                String.format("%1$2s", day)
+        );
+        date = ft.parse(builder);
     }
 
     public boolean isWeekend() {
-        // TODO: implement
-        return false;
+        // TODO
+        return this.date.getDay() == 6 || this.date.getDay() == 7;
     }
 
     public boolean isHoliday() {
-        // TODO: implement
+        // TODO
         return false;
-    }
-
-
-    public Week getWeek() {
-        return week;
     }
 
     public int getDay0fWeek() {
