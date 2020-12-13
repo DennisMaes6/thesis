e3ac8edc32fa9fc9f922b15e9e502dd579de22b8
joren.sjongers <joren.sjongers@icloud.com>
2020-12-13 12:18:13 +0100

    remove redundant exception

--- src/com/scheduler/exceptions/AssignWholeWeeksException.java
+++
@@ -1,7 +0,0 @@
-package com.scheduler.exceptions;
-
-public class AssignWholeWeeksException extends Exception {
-    public AssignWholeWeeksException(String errorMessage) {
-        super(errorMessage);
-    }
-}
