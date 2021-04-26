c4a01cb2f1d89c9c5cccc3b50f9149ed748294dc
joren.sjongers <joren.sjongers@icloud.com>
2021-04-26 21:58:49 +0200

    use real instance db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,39 @@
 import exceptions.DbControllerException;
 import exceptions.NoSuitableAssistantException;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException, DbControllerException, NoSuitableAssistantException {
+    public static void main(String[] args) throws SQLException, DbControllerException, NoSuitableAssistantException, IOException {
 
-        String dbPath = System.getProperty("user.home") + "/scheduler/backend/sqlite-database-new.db";
+        String dbPath = System.getProperty("user.home") + "/scheduler/backend/real-instance.db";
         DbController controller = new DbController(dbPath);
 
+        FileWriter writer = new FileWriter("result.csv");
         Algorithm algorithm = new Algorithm(controller.getInstanceData(), controller.getModelParameters());
-        Schedule schedule = algorithm.generateSchedule();
 
+        for (int i = 0; i < 1000; i++) {
+            long startTime = System.nanoTime();
+            Schedule schedule = algorithm.generateSchedule();
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+            writer.append(String.format("%f, %f \n", schedule.fairnessScore(), duration/1000000.0));
+        }
+        writer.flush();
+        writer.close();
+
+/*
         System.out.println("fairness: " + schedule.fairnessScore());
         System.out.println("balance: " + schedule.balanceScore());
         System.out.println(schedule.toString());
 
-        controller.putSchedule(schedule);
+ */
+
+        //controller.putSchedule(schedule);
     }
 }
